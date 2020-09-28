869a4e256b87b9b24ebc5cf717d2aae4b54c26c8
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-09-28 12:10:52 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -17,3 +17,13 @@
 # "2" : The number is 100 or a number up to 1000 
 # "1" : The number is 10 or a number up to 100 
 # "0" : The number is 0 or a number up to 100 
+a=input("enter in number")
+a=float(a)
+if a>1000:
+    print(3)
+elif a>100:
+    print(2)
+elif a>10:
+    print(1)
+elif a>0:
+    print(0)
