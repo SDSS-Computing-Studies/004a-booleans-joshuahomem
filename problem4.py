97a7536b24616529f70bb033f8d467cc5953973a
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-09-30 12:45:31 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,3 +12,28 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
+a=input("enter short side 1 ")
+b=input("enter short side 2 ")
+c=input("enter hypotenuse ")
+a=float(a)
+b=float(b)
+c=float(c)
+
+side1=a**2
+side2=b**2
+side3=c**2
+value1=side1+side2
+number1=value1*0.02
+range1=value1+number1
+range2=value1-number1
+
+if side3>range2 and side3<range1:
+    print("that is a right triangle")
+
+
+
+elif side3>side1+side2:
+    print("that is an obuse triangle")
+elif side3<side1+side2:
+    print("that in an acute triangle")
+
